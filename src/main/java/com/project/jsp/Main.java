c3ccce702287864dbb1db6402e8bc619a9fadb3c
lkwnghn@gmail.com <lkwnghn@gmail.com>
2022-09-09 20:59:16 +0900

    DBCP 연결 및 수정

--- src/main/java/com/project/jsp/Main.java
+++ src/main/java/com/project/jsp/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.project.jsp;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/com/project/jsp/Servlet.java
+++ src/main/java/com/project/jsp/Servlet.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.project.jsp;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,15 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ServletTest", urlPatterns = "/test2")
+@WebServlet(name = "ServletTest", urlPatterns = "/test")
+// @WebServlet 어노테이션을 통해
+// localhost/8181/test 를 치게되면, doGet() 메서드를 통해 페이지를 보여준다.
+// .jsp로 굳이 파일을 안열어도 된다.
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         PrintWriter out = response.getWriter();
-        out.println("<p>Servlet Test</p>");
+        out.println("<p>Web Servlet 어노테이션 활용방법</p>");
     }
 }
