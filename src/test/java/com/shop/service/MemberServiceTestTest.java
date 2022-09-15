279a78de0c92a43450c4e34938aa36da29a4da3c
leekwah <>
2022-09-16 00:15:04 +0900

    itemImg

---
+++ src/test/java/com/shop/service/MemberServiceTestTest.java
@@ -0,0 +1,88 @@
+package com.shop.service;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+class MemberServiceTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PersistenceContext
+    EntityManager em;
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest(){
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getAddress(), savedMember.getAddress());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getRole(), savedMember.getRole());
+    }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest(){
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);});
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
+
+    @Test
+    @DisplayName("Auditing 테스트")
+    @WithMockUser(username = "gildong", roles = "USER")
+    public void auditingTest(){
+        Member newMember = new Member();
+        memberRepository.save(newMember);
+
+        em.flush();
+        em.clear();
+
+        Member member = memberRepository.findById(newMember.getId()).orElseThrow(EntityNotFoundException::new);
+
+        System.out.println("register time : " + member.getRegTime());
+        System.out.println("update time : " + member.getUpdateTime());
+        System.out.println("create member : " + member.getCreatedBy());
+        System.out.println("modify member : " + member.getModifiedBy());
+    }
+
+}
