0318a1e516e114fd41d734d490bcda2f851c8be0
leekwah <>
2022-09-16 02:45:41 +0900

    itemImgService + Controller

--- src/main/java/com/shop/dto/ItemImgDto.java
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -12,14 +12,16 @@ public class ItemImgDto {
 
     private String imgName;
 
+    private String oriImgName;
+
     private String imgUrl;
 
     private String repImgYn;
 
     private static ModelMapper modelMapper = new ModelMapper();
 
-    public static ItemImgDto of(ItemImg itemImg){
-        return modelMapper.map(itemImg, ItemImgDto.class);
+    public static ItemImgDto of(ItemImg itemImg) {
+        return modelMapper.map(itemImg,ItemImgDto.class);
     }
 
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -12,32 +12,57 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import com.shop.dto.ItemImgDto;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
     private final ItemImgService itemImgService;
+
     private final ItemImgRepository itemImgRepository;
 
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
 
-        // 상품 등록
+        //상품 등록
         Item item = itemFormDto.createItem();
         itemRepository.save(item);
 
-        // 이미지 등록
-        for (int i = 0; i < itemImgFileList.size(); i++){
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
             ItemImg itemImg = new ItemImg();
             itemImg.setItem(item);
-            if (i == 0)
+
+            if(i == 0)
                 itemImg.setRepimgYn("Y");
             else
                 itemImg.setRepimgYn("N");
+
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
         }
 
         return item.getId();
     }
+
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId){
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
+
 }
