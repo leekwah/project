d2a6e5c9ea36fdc4a878ee71ec5ba4958e0f54e3
lkwnghn@gmail.com <lkwnghn@gmail.com>
2022-10-12 20:21:46 +0900

    First Commit

---
+++ src/main/java/com/kwah/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.kwah.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
