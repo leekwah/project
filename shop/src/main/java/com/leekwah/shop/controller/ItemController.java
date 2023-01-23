13d3c3d0808455c49bd2868e7df5ba99df76c7ae
leekwah <kwah.lkh@gmail.com>
2023-01-23 21:15:40 +0900

    ItemController

---
+++ shop/src/main/java/com/leekwah/shop/controller/ItemController.java
@@ -0,0 +1,13 @@
+package com.leekwah.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+
+    @GetMapping(value = "/admin/item/net")
+    public String itemForm() {
+        return "/item/itemForm";
+    }
+}
