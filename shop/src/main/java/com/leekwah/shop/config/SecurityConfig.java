6927eb5942e346e22692a8b4c3928a840ce3f7f6
leekwah <kwah.lkh@gmail.com>
2023-01-24 10:13:34 +0900

    CustomAuthenticationEntryPoint 생성, SpringSecurity 설정추가

--- shop/src/main/java/com/leekwah/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/leekwah/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                 .logoutSuccessUrl("/");
+
+        http.authorizeRequests() // 시큐리티 처리에 HttpServletRequest 를 이용한다는 걸 의미
+                .mvcMatchers("/", "/members/**", "/item/**", "images/**").permitAll() // permitAll() 메서드를 통해서 인증(로그인)없이 경로에 접근가능
+                .mvcMatchers("/admin/**").hasRole("ADMIN") // /admin 으로 설정해둔 경로는 ADMIN ROLE이 있어야 가능
+                .anyRequest().authenticated();
+
+        http.exceptionHandling() // 인증되지 않은 사용자가 리소스에 접근시 수행되는 핸들러 등록
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
     }
 
     @Bean
@@ -41,6 +50,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(memberService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception { // static 디렉터리 하위 파일은 인증 무시하도록 설정
+        web.ignoring().antMatchers("/css/**", "/js/**", "img/**");
     }
 }
 
