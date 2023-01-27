73e47c1823b34aa40b6527c70aea8bec37e321e7
leekwah <kwah.lkh@gmail.com>
2023-01-27 23:02:43 +0900

    itemService 작성

--- shop/src/main/java/com/leekwah/shop/service/ItemService.java
+++ shop/src/main/java/com/leekwah/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.leekwah.shop.service;
 
 import com.leekwah.shop.dto.ItemFormDto;
+import com.leekwah.shop.dto.ItemImgDto;
 import com.leekwah.shop.entity.Item;
 import com.leekwah.shop.entity.ItemImg;
 import com.leekwah.shop.repository.ItemImgRepository;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -42,5 +45,23 @@ public class ItemService {
         }
 
         return item.getId();
+    }
+
+    @Transactional(readOnly = true) // 상품 데이터를 읽어오는 트랜잭션을 읽기 전용으로 설정한다. 이럴 경우 JPA 가 더티체킹(변경감지)을 수행하지 않아서 성능을 향상시킬 수 있다.
+    public ItemFormDto getItemDtl(Long itemId) {
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+
+        return itemFormDto;
     }
 }
