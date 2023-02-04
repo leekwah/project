344c85fcf0b0d193ef6639ffb7f26d00dc733ac2
leekwah <kwah.lkh@gmail.com>
2023-02-04 22:42:19 +0900

    장바구니 기능

--- shop/src/main/java/com/leekwah/shop/service/CartService.java
+++ shop/src/main/java/com/leekwah/shop/service/CartService.java
@@ -1,5 +1,10 @@
 package com.leekwah.shop.service;
 
+import com.leekwah.shop.dto.CartItemDto;
+import com.leekwah.shop.entity.Cart;
+import com.leekwah.shop.entity.CartItem;
+import com.leekwah.shop.entity.Item;
+import com.leekwah.shop.entity.Member;
 import com.leekwah.shop.repository.CartItemRepository;
 import com.leekwah.shop.repository.CartRepository;
 import com.leekwah.shop.repository.ItemRepository;
@@ -7,6 +12,8 @@ import com.leekwah.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +25,28 @@ public class CartService {
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
 
+    public Long addCart(CartItemDto cartItemDto, String email) { // 장바구니에 담을 상품 엔티티를 조회한다.
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
 
+        Member member = memberRepository.findByEmail(email); // 현재 로그인한 회원 엔티티를 조회한다.
+
+        Cart cart = cartRepository.findByMemberId(member.getId()); // 현재 로그인한 히원의 장바구니 엔티티를 조회한다.
+        if (cart == null) { // 상품을 처음으로 장바구니에 담을 경우 해당 회원의 장바구니 엔티티를 생성한다.
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId()); // 현재 상품이 장바구니에 이미 들어가 있는지 조회한다.
+
+        if (savedCartItem != null) {
+            savedCartItem.addCount(cartItemDto.getCount()); // 장바구니에 이미 있던 상품일 경우, 기존 수량에 현재 장바구니에 담을 수량 만큼을 더해준다.
+            return savedCartItem.getId();
+        } else {
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount()); // 장바구니 엔티티, 상품 엔티티, 장바구니에 담을 수량을 이용하여 CartItem 엔티티를 생성한다.
+            cartItemRepository.save(cartItem); // 장바구니에 들어갈 상품을 저장한다.
+            return cartItem.getId();
+        }
+
+    }
 }
